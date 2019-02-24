9c80f1b879b6e3464398d102837ddbfefc847972
jakub_mottl <mottl.jakub.janov@gmail.com>
2019-02-24 12:13:36 +0100

    Code refomrat

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ use Tracy\Debugger;
 Debugger::enable(Debugger::DETECT, __DIR__ . '/log');
 
 if (($_SERVER['REQUEST_METHOD'] === 'GET')
-	and ((array_key_exists('hub_challenge', $_GET))and(array_key_exists('hub_verify_token', $_GET)))
+	and ((array_key_exists('hub_challenge', $_GET)) and (array_key_exists('hub_verify_token', $_GET)))
 	and ($_GET['hub_verify_token'] === VERIFY_TOKEN)) {
 	echo $_GET['hub_challenge'];
 }
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$input = file_get_contents('php://input');
 	//$input = file_put_contents(__DIR__ . '/tmp/message.json');
 
-	if ($input){
+	if ($input) {
 		$request = Json::decode($input, Json::FORCE_ARRAY);
 
 		$bot = new Bot(ACCESS_TOKEN, BOT_ID);
